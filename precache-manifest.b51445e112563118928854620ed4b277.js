019bbee491d2eaf378dfe5dd76538f2937239a2a
Tayyab94 <Tayyab.bhatti30@gmail.com>
2022-06-23 07:04:50 -0700

    Updates

---
+++ precache-manifest.b51445e112563118928854620ed4b277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85d5645ea9db62bfe31e",
+    "url": "/React-TextUtils/static/css/main.b09f05c8.chunk.css"
+  },
+  {
+    "revision": "85d5645ea9db62bfe31e",
+    "url": "/React-TextUtils/static/js/main.85d5645e.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/React-TextUtils/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "0b92b2cdee985b12436a",
+    "url": "/React-TextUtils/static/js/2.0b92b2cd.chunk.js"
+  },
+  {
+    "revision": "8a420443fe8cc7adadca",
+    "url": "/React-TextUtils/static/js/runtime~main.8a420443.js"
+  },
+  {
+    "revision": "8fb5b780bc758d44c429fb4f871cab23",
+    "url": "/React-TextUtils/index.html"
+  }
+];
